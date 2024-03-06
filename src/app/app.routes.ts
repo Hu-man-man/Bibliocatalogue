9f6de8862ef99bc451bf4fedf03796bdc88cc62b
Jason <jason.ricou@gmail.com>
2024-03-06 09:28:55 +0100

    suppression de routes qui faisaint doublon et réinstallation des dépendanses

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,5 @@ import { LoginComponent } from "./login/login.component";
 import { authGuard } from "./auth.guard";
 
 export const routes: Routes = [
-  { path: "", redirectTo: "login", pathMatch: "full" },
-  { path: "login", component: LoginComponent },
-  { path: "book-list", canActivate: [authGuard], component: BookListComponent },
+  { path: "book-list", /*canActivate: [authGuard],*/ component: BookListComponent },
 ];
